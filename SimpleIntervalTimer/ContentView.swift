33d004013c070c55012b98719d656349c6407a99
Robin Börjesson <robin.b@outlook.com>
2024-01-07 13:12:49 +0100

    Initial Commit

---
+++ SimpleIntervalTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleIntervalTimer
+//
+//  Created by Robin Börjesson on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleIntervalTimer/SimpleIntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleIntervalTimerApp.swift
+//  SimpleIntervalTimer
+//
+//  Created by Robin Börjesson on 2024-01-07.
+//
+
+import SwiftUI
+
+@main
+struct SimpleIntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
